418eab331433e149b004abd07e7bd2b98a62af06
Sridhar Mocherla <smocherla@brex.com>
2025-03-25 12:11:30 -0400

    Fix aspect version

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const ASPECT_RELEASE_VERSION = "0.0.1-rc.3"
+export const ASPECT_RELEASE_VERSION = "v0.0.1-rc.3"
 export const ASPECT_RELEASE_ARCHIVE_SHA256: Record<string, string> ={
     "v0.0.1-rc.3": "d67de29d94a487e6120ab97d287d99bdf422eede096e5c0929a63768e6e7d741"
 }
