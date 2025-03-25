dfd443f030f2a9233423a07ce5282c1a5d3d8415
Sridhar Mocherla <smocherla@brex.com>
2025-03-25 12:05:29 -0400

    bump aspect version with fixes

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
-export const ASPECT_RELEASE_VERSION = "v0.0.1-rc.1"
+export const ASPECT_RELEASE_VERSION = "0.0.1-rc.3"
 export const ASPECT_RELEASE_ARCHIVE_SHA256: Record<string, string> ={
-    "v0.0.1-rc.1": "a05816e4eb27365f7ba6d5ecd977f3253444e72767098338f512827e2c4444f1"
+    "v0.0.1-rc.3": "d67de29d94a487e6120ab97d287d99bdf422eede096e5c0929a63768e6e7d741"
 }
 
 export const KLS_RELEASE_ARCHIVE_SHA256: Record<string, string> ={
