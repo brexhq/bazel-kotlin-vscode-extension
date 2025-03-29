c944e54b8c6b111d995eac7cc5741ea5f8053cf7
Sridhar Mocherla <smocherla@brex.com>
2025-03-29 12:34:48 -0400

    Update extension version

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,10 @@
-export const ASPECT_RELEASE_VERSION = "v0.0.2-rc.1"
-export const ASPECT_RELEASE_ARCHIVE_SHA256: Record<string, string> ={
-    "v0.0.2-rc.1": "b6e50b4a5eddca4f2879fa5fef79e3f661af006c53449dbd7e96d1520e97a714"
-}
+export const ASPECT_RELEASE_VERSION = "v0.0.2-rc.1";
+export const ASPECT_RELEASE_ARCHIVE_SHA256: Record<string, string> = {
+  "v0.0.2-rc.1":
+    "b6e50b4a5eddca4f2879fa5fef79e3f661af006c53449dbd7e96d1520e97a714",
+};
 
-export const KLS_RELEASE_ARCHIVE_SHA256: Record<string, string> ={
-    "v1.3.14-bazel": "39eda21ef69e448ed9738c35993158f797f0817e8d56ff44abbd7c6981197251"
-}
+export const KLS_RELEASE_ARCHIVE_SHA256: Record<string, string> = {
+  "v1.3.14-bazel":
+    "39eda21ef69e448ed9738c35993158f797f0817e8d56ff44abbd7c6981197251",
+};
