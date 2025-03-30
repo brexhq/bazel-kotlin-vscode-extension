88e2c176ef0ea4d9a9fbe9bec45639a1464c2ad9
Sridhar Mocherla <smocherla@brex.com>
2025-03-30 13:40:03 -0400

    Bump LSP version (#20)
    * Bump LSP version
    
    To include bug fix for goto https://github.com/smocherla-brex/kotlin-language-server-bazel-support/pull/18
    
    * Update package.json
    
    * Bump extension version
    
    * update shas
    
    * fix test

--- src/constants.ts
+++ src/constants.ts
@@ -7,4 +7,6 @@ export const ASPECT_RELEASE_ARCHIVE_SHA256: Record<string, string> = {
 export const KLS_RELEASE_ARCHIVE_SHA256: Record<string, string> = {
   "v1.3.14-bazel":
     "39eda21ef69e448ed9738c35993158f797f0817e8d56ff44abbd7c6981197251",
+  "v1.3.15-bazel": 
+    "6fcb725ec0aaecd5dfb14af40e4245f2f9c1c22d1f57bee86f812d0599d5616e",  
 };
