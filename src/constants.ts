f67446e7478b686850b0c292b1b3c06ba4495eb9
Sridhar Mocherla <smocherla@brex.com>
2025-04-01 12:22:01 -0400

    Bump lsp and aspect version (#23)
    * Bump lsp and aspect version
    
    * fix test
    
    * Fixes

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
-export const ASPECT_RELEASE_VERSION = "v0.0.2-rc.3";
+export const ASPECT_RELEASE_VERSION = "v0.0.3";
 export const ASPECT_RELEASE_ARCHIVE_SHA256: Record<string, string> = {
-  "v0.0.2-rc.3":
-    "59c8b213d8a4bc3f818d6f88cd0eaff0439a1b7c2c90065c798e671a2e8765f1",
+  "v0.0.3":
+    "f369532b7225fa409ff31e89e674dcfb2d4b12a4f589fc68863f8da76424fc1a",
 };
 
 export const KLS_RELEASE_ARCHIVE_SHA256: Record<string, string> = {
@@ -9,4 +9,6 @@ export const KLS_RELEASE_ARCHIVE_SHA256: Record<string, string> = {
     "39eda21ef69e448ed9738c35993158f797f0817e8d56ff44abbd7c6981197251",
   "v1.3.15-bazel": 
     "6fcb725ec0aaecd5dfb14af40e4245f2f9c1c22d1f57bee86f812d0599d5616e",  
+   "v1.3.16-bazel":
+    "ad9912882298cfc8e3e25f7dd16b2c5c1d683fb4d718f42044720388586e0a74", 
 };
