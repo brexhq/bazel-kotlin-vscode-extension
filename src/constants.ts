0337c26c411ed5ae201248604c061a4b624f3080
Sridhar Mocherla <smocherla@brex.com>
2025-04-02 19:44:17 -0400

    Bump version (#26)

--- src/constants.ts
+++ src/constants.ts
@@ -1,14 +1,17 @@
 export const ASPECT_RELEASE_VERSION = "v0.0.4";
 export const ASPECT_RELEASE_ARCHIVE_SHA256: Record<string, string> = {
-  "v0.0.4":
-    "135a16dbe18b40764fb0e8ec8f0bd7f0ae3f73c2e71f049341ef7ba352b78d6a",
+  "v0.0.4": "135a16dbe18b40764fb0e8ec8f0bd7f0ae3f73c2e71f049341ef7ba352b78d6a",
 };
 
 export const KLS_RELEASE_ARCHIVE_SHA256: Record<string, string> = {
   "v1.3.14-bazel":
     "39eda21ef69e448ed9738c35993158f797f0817e8d56ff44abbd7c6981197251",
-  "v1.3.15-bazel": 
-    "6fcb725ec0aaecd5dfb14af40e4245f2f9c1c22d1f57bee86f812d0599d5616e",  
-   "v1.3.16-bazel":
-    "ad9912882298cfc8e3e25f7dd16b2c5c1d683fb4d718f42044720388586e0a74", 
+  "v1.3.15-bazel":
+    "6fcb725ec0aaecd5dfb14af40e4245f2f9c1c22d1f57bee86f812d0599d5616e",
+  "v1.3.16-bazel":
+    "ad9912882298cfc8e3e25f7dd16b2c5c1d683fb4d718f42044720388586e0a74",
+  "v1.3.17-bazel":
+    "e2302bb80902aa47203325268037c0f8793630e80261dca4d00272e61c9cde0f",
+  "v1.3.18-bazel":
+    "cf8ea6d7f11415957d0bd538d34b7dd199697a7596e6a356b57ec2446331e574",
 };
